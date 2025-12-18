a341a839f96c52166c7276ec2a2d15d12286c5d1
BAC <balaianggunceria@gmail.com>
2025-12-18 23:29:06 +0700

    cook

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const local = 'http://localhost:5000'
-const production = 'https://bac-backend-web-1.onrender.com'
+const production = 'https://bac-backend-web.onrender.com'
 
 let api_url = ''
 let mode = 'pro'
